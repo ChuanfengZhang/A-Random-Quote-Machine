fd2723aa61a40e7acb3b029bcfbc44e4058cf29f
Chuanfeng Zhang <wfhtzcf@gmail.com>
2016-10-09 11:20:51 +0800

    Add auto-get quote function

---
+++ js/index.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#message',
+    data: {
+        message: 'test',
+        url: 'https://codepen.io/Chuanfeng/full/BLWPBO'
+    },
+    ready: function() {
+        this.getApi();
+    },
+    methods: {
+        getApi: function() {
+            $('#message').fadeOut();
+            this.message = $.ajax({
+                url: "https://api.lwl12.com/hitokoto/main/get",
+                async: false
+            }).responseText;
+            $('#message').fadeIn();
+        }
+    }
+})
